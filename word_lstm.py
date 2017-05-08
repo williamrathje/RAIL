bc72cd91060051fd041c63db98d39d456d1eb49b
William Rathje <bwjr@nwlink.com>
2017-05-08 11:01:32 +0100

    Update word_lstm.py

--- word_lstm.py
+++ word_lstm.py
@@ -300,7 +300,7 @@ class CustomConfig(object):
   lr_decay = 0.5
   batch_size = 20
   #vocab_size = 124262
-  vocab_size = 164144
+  vocab_size = 171488
 
 
 def run_epoch(session, model, eval_op=None, verbose=False):
