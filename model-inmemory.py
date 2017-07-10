7f3023bcdc40afd235551a086bbd566cdba47f2e
William Rathje <bwjr@nwlink.com>
2017-07-10 11:29:03 +0100

    Update model-inmemory.py

--- model-inmemory.py
+++ model-inmemory.py
@@ -49,7 +49,7 @@ def prepare(text):
 print('Build model...')
 model = Sequential()
 #model.add(LSTM(700, return_sequences=True, input_shape=(maxlen, len(chars))))
-model.add(LSTM(700, return_sequences=False, input_shape=(maxlen, len(chars))))
+model.add(LSTM(800, return_sequences=False, input_shape=(maxlen, len(chars))))
 model.add(Dropout(0.65))
 #model.add(LSTM(700))
 #model.add(Dropout(0.65))
